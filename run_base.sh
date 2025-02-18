38b703cd87d9a7dcab0a8f2c5bfd51fbae79fe96
Yuhang Li <Apple@192.168.5.14>
2025-02-18 09:39:47 +0800

    update bash files

--- run_base.sh
+++ run_base.sh
@@ -14,8 +14,12 @@ time=$(date +"%Y-%m-%d %H:%M:%S")
 
 if [ ${distr} -eq 1 ]; then
     file_path="./results/base/${method}/${dataset}_${distr}_${seed}.log"
+    log_dir=$(dirname "${file_path}")
+    mkdir -p "${log_dir}"
     nohup python -u ${method}.py -dataset ${dataset} -distr ${distr} -lr ${lr} -gpu ${gpu} -bs ${bs} -seed ${seed} > ${file_path} 2>&1 &
 else
     file_path="./results/base/${method}/${dataset}_${distr}_${nc}_${seed}.log"
+    log_dir=$(dirname "${file_path}")
+    mkdir -p "${log_dir}"
     nohup python -u ${method}.py -dataset ${dataset} -distr ${distr} -nc ${nc} -lr ${lr} -gpu ${gpu} -bs ${bs} -seed ${seed} > ${file_path} 2>&1 &
 fi
